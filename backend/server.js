d7eea4c0eca86c81b145ec2b8e0f1b0ffef29316
Winn <winnieee.lam00@gmail.com>
2023-08-31 16:59:55 -0400

    Set up backend express server and database

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { sequelize } from "./datasource.js";
+
+export const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+try {
+  await sequelize.authenticate();
+  await sequelize.sync({ alter: { drop: false } });
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}
+
+const PORT = 5000;
+app.listen(PORT, (err) => {
+  if (err) console.log(err);
+  else console.log("HTTP server on http://localhost:%s", PORT);
+});
